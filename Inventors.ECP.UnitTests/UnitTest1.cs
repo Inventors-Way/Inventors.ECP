496d930638c2ebdbfbfa8e51aacf827a4d44b593
Kristian Hennings <krhe@inventors.dk>
2020-01-18 17:06:07 +0100

    Added a unittest project

---
+++ Inventors.ECP.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inventors.ECP.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
