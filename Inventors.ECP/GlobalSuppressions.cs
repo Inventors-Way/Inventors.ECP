1b174097605ac2bd1fe5de26406bc787a99c0a7b
Kristian Hennings <krhe@inventors.dk>
2020-02-01 20:04:27 +0100

    Update GlobalSuppressions.cs

--- Inventors.ECP/GlobalSuppressions.cs
+++ Inventors.ECP/GlobalSuppressions.cs
@@ -2,6 +2,4 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "<Pending>", Scope = "type", Target = "~T:Inventors.ECP.Function")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "type", Target = "Inventors.ECP")]
