dc4b9c14f309ae6fbcd9eaeda08114f5daa590d8
Kristian Hennings <krhe@inventors.dk>
2021-05-06 15:09:07 +0200

    Cleanup and function and message stubs

--- Inventors.ECP.UnitTests/ConsoleLog.cs
+++ Inventors.ECP.UnitTests/ConsoleLog.cs
@@ -13,8 +13,12 @@ namespace Inventors.ECP.UnitTests
 
         public void Add(DateTime time, LogLevel level, string str) => Console.WriteLine($"[{level}: {str}");
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Required by interface")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Required by interface")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required by interface")]
         public void AddMonitor(ILogger log)  {}
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Required by interface")]
         public void Initialize() { }
 
         [AssemblyInitialize]
