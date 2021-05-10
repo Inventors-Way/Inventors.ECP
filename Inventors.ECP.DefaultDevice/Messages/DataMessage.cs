d3b341b3e9eef574b452505c1b051f684198cf04
Kristian Hennings <krhe@inventors.dk>
2021-05-10 10:27:21 +0200

    Started implementing test functions and messages

--- Inventors.ECP.DefaultDevice/Messages/DataMessage.cs
+++ Inventors.ECP.DefaultDevice/Messages/DataMessage.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Inventors.ECP.DefaultDevice.Messages
 {
-    class DataMessage
+    public class DataMessage :
+        DeviceMessage
     {
+        public static readonly byte CODE = 0x80;
+
+        public override byte Code => CODE;
+
+        public DataMessage() : base(CODE, length: 0)
+        {
+        }
+        
+        public DataMessage(Packet response) :
+            base(response)
+        {
+            if (Packet.Length == 0)
+                throw new InvalidMessageException($"A received DataMessage does not contain any data");
+
+            if (Packet.Length % sizeof(int) != 0)
+                throw new InvalidMessageException("A received DataMessages does not contain a whole number of ints");
+        }
+
+        public List<int> Data { get; } = new List<int>();
+
+        public override void OnSend()
+        {
+            Packet = new Packet(CODE, sizeof(int) * Data.Count);
+
+            for (int n = 0; n < Data.Count; ++n)
+            {
+                Packet.InsertInt32(n * sizeof(int), Data[n]);
+            }
+        }
+
+        public override void OnReceived()
+        {
+            int noOfPoints = Packet.Length / sizeof(int);
+            Data.Clear();
+
+            for (int n = 0; n < noOfPoints; ++n)
+            {
+                Data.Add(Packet.GetInt32(n * sizeof(int)));
+            }
+        }
+
+        public override MessageDispatcher CreateDispatcher() => new MessageDispatcher(CODE, (p) => new DataMessage(p));
+
+        public override void Dispatch(dynamic listener) => listener.Accept(this);
     }
 }
--- Inventors.ECP.DefaultDevice/Messages/SimpleMessage.cs
+++ Inventors.ECP.DefaultDevice/Messages/SimpleMessage.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Inventors.ECP.DefaultDevice.Messages
 {
-    class SimpleMessage
+    public class SimpleMessage :
+        DeviceMessage
     {
+        public static readonly byte CODE = 0x80;
+
+        public override byte Code => CODE;
+
+        public SimpleMessage() : base(CODE, sizeof(int))
+        {
+            X = 0;
+        }
+
+        public SimpleMessage(Packet response) :
+            base(response)
+        {
+            if (Packet.Length != 2 * sizeof(UInt16))
+            {
+                throw new InvalidMessageException($"A received ClickMessage does not have a length of {2 * sizeof(UInt16)}");
+            }
+        }
+
+        public int X
+        {
+            get => Packet.GetInt32(0);
+            set => Packet.InsertInt32(0, value);
+        }
+
+        public override MessageDispatcher CreateDispatcher() => new MessageDispatcher(CODE, (p) => new SimpleMessage(p));
+
+        public override void Dispatch(dynamic listener) => listener.Accept(this);
     }
 }
