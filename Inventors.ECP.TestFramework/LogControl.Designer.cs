48d65b9bbbbf54a662d52da45c06a07cf21d97e2
Kristian Hennings <krhe@inventors.dk>
2024-11-25 17:37:58 +0100

    UI Adjustment

--- Inventors.ECP.TestFramework/LogControl.Designer.cs
+++ Inventors.ECP.TestFramework/LogControl.Designer.cs
@@ -28,51 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.logEntry = new System.Windows.Forms.TextBox();
-            this.logBox = new System.Windows.Forms.TextBox();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            logEntry = new System.Windows.Forms.TextBox();
+            logBox = new System.Windows.Forms.TextBox();
+            timer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
             // 
             // logEntry
             // 
-            this.logEntry.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.logEntry.Location = new System.Drawing.Point(0, 480);
-            this.logEntry.Margin = new System.Windows.Forms.Padding(4);
-            this.logEntry.Name = "logEntry";
-            this.logEntry.Size = new System.Drawing.Size(1048, 22);
-            this.logEntry.TabIndex = 1;
-            this.logEntry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LogEntry_KeyDown);
+            logEntry.Dock = System.Windows.Forms.DockStyle.Bottom;
+            logEntry.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            logEntry.Location = new System.Drawing.Point(0, 446);
+            logEntry.Margin = new System.Windows.Forms.Padding(4);
+            logEntry.Name = "logEntry";
+            logEntry.Size = new System.Drawing.Size(917, 25);
+            logEntry.TabIndex = 1;
+            logEntry.KeyDown += LogEntry_KeyDown;
             // 
             // logBox
             // 
-            this.logBox.BackColor = System.Drawing.Color.White;
-            this.logBox.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logBox.Location = new System.Drawing.Point(0, 0);
-            this.logBox.Multiline = true;
-            this.logBox.Name = "logBox";
-            this.logBox.ReadOnly = true;
-            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.logBox.Size = new System.Drawing.Size(1045, 473);
-            this.logBox.TabIndex = 2;
+            logBox.BackColor = System.Drawing.Color.White;
+            logBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            logBox.Location = new System.Drawing.Point(0, 0);
+            logBox.Multiline = true;
+            logBox.Name = "logBox";
+            logBox.ReadOnly = true;
+            logBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            logBox.Size = new System.Drawing.Size(915, 444);
+            logBox.TabIndex = 2;
             // 
             // timer
             // 
-            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            timer.Tick += Timer_Tick;
             // 
             // LogControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.logBox);
-            this.Controls.Add(this.logEntry);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "LogControl";
-            this.Size = new System.Drawing.Size(1048, 502);
-            this.SizeChanged += new System.EventHandler(this.LogControl_SizeChanged);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(logBox);
+            Controls.Add(logEntry);
+            Margin = new System.Windows.Forms.Padding(4);
+            Name = "LogControl";
+            Size = new System.Drawing.Size(917, 471);
+            SizeChanged += LogControl_SizeChanged;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
